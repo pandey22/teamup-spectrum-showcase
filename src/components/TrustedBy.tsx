2a57e1f90de4f725525eb523711643fc0129ecb4
Kshitij Pandey <kshitijpandey25@gmail.com>
2025-08-10 08:06:51 +0530

    Added logos for industry leaders

--- src/components/TrustedBy.tsx
+++ src/components/TrustedBy.tsx
@@ -7,7 +7,7 @@ const TrustedBy = () => {
   ];
 
   return (
-    <section className="py-16 bg-background border-b border-border/50">
+    <section className="py-16 bg-background">
       <div className="container mx-auto px-6">
         <div className="text-center mb-12 animate-fade-in">
           <Badge variant="outline" className="mb-4">
@@ -23,7 +23,7 @@ const TrustedBy = () => {
           </p>
         </div>
 
-        <div className="flex flex-wrap justify-center items-center gap-8 lg:gap-12 animate-fade-in" style={{ animationDelay: '0.2s' }}>
+        {/* <div className="flex flex-wrap justify-center items-center gap-8 lg:gap-12 animate-fade-in" style={{ animationDelay: '0.2s' }}>
           {companies.map((company, index) => (
             <div 
               key={company}
@@ -33,6 +33,17 @@ const TrustedBy = () => {
               {company}
             </div>
           ))}
+        </div> */}
+        <div className="flex items-center gap-40 justify-center flex-wrap mt-8 animate-fade-in">
+          <img src="./src/assets/qualcomm.svg" alt="qualcomm" className="w-20 h-auto"/>
+          <img src="./src/assets/verizon.svg" alt="Verizon" className="w-20 h-auto"/>
+          <img src="./src/assets/samsung.svg" alt="samsung" className="w-20 h-auto"/>
+          <img src="./src/assets/commscope.svg" alt="commScope" className="w-20 h-auto "/>
+
+          <img src="./src/assets/qualcomm.svg" alt="commScope" className="w-20 h-auto"/>
+
+          
+
         </div>
       </div>
     </section>
